81edc8dc090a2ae5610a60eeadb70b9be1ac84f0
FIGUEROA9 <pf.figueroa9@gmail.com>
2025-11-13 20:41:29 -0500

    primer commit de script

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Java!");
+
+        int a = 5;
+        int b = 10;
+        int suma = a + b;
+        System.out.println("La suma de " + a + " + " + b + " = " + suma);
+    }
+}
+
