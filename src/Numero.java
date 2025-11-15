c077813fa8633ab47fc6be26396acdb8e3ae3db8
FIGUEROA9 <pf.figueroa9@gmail.com>
2025-11-14 19:32:44 -0500

    incluyo cuarto archivo

---
+++ src/Numero.java
@@ -0,0 +1,13 @@
+public class Numero{
+    public static void main(String[] args) {
+        int numero = 10;
+
+        if (numero % 2 == 0) {
+            System.out.println(numero + " es par");
+        } else {
+            System.out.println(numero + " es impar");
+        }
+    }
+}
+
+
